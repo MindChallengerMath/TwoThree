41ccc6c130ac9caedf2b97e3e70c3efaa94d6a46
MindChallengerMath <mindchallengermath@gmail.com>
2026-04-02 16:44:42 -0400

    Finished game

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+#Dice roller program.
 import random
 import math
 #● ┌ ─ ┐ │ └ ┘
@@ -40,6 +41,13 @@ dice = []
 cash = 1000
 bet = None
 wager = 0
+forced_bet = 0
+dice_rolled_one = 0
+dice_rolled_two = 0
+dice_rolled_three = 0
+dice_rolled_four = 0
+dice_rolled_five = 0
+dice_rolled_six = 0
 def dealing():
         for dealer_die in range(random.randint(0,2)):
            dice.append(random.randint(1,6))
@@ -65,54 +73,101 @@ def pick_side(player_choice):
                 print("That is not an option.")
                 player_choice = input("Pick 1-6 please: ")
         return player_choice
+        
 def select_again(selection):
         while selection.isdigit() == False:
                print("That is not a number.")
                selection = input("Pick 2 or 3: ")
-        while not selection == "2" and not selection == "3":
-            print("That is not an option.")
-            selection = input("Pick 2 or 3: ")
+        while not selection == "2"  and not selection == "3":
+                print("That is not an option.")
+                selection = input("Pick 2 or 3: ")
         return selection
 def gambling(waging, money):
-    while True:
-        waging = input("Place wager: ")
-        try:
-            val = int(waging)
-            if val <= money and val > 0:
-                break
-            else:
-                print("Value invalid")
-        except ValueError:
-            print("Your wager must be an interger.")
+        while True:
+                waging = input("Enter your wager: ")
+                try:
+                                val = int(waging)
+                                if val <= money and val > 0:
+                                        break
+                                else:
+                                        print("Value Invalid.")
+                except ValueError:
+                        print("Your wager must be an interger.")
+
+        
         return waging
-def winning(dice, given_bet)
-    win_multiplier = 0
-    if dice.count(given_bet) == 2:
-        win_multiplier += 2
-    elif dice.count(given_bet) >= 3:
-        win_multiplier += 3
-    else:
-        print("You lose ):")
-    return win_multiplier
-while running:
-    print(cash)
-    player_side = None
-    player_wager = 0
-    amount_player_won = 0
-    dice.clear()
-    dealing()
-    player_side = int(pick_side(bet))
-    player_wager = int(gambling(wager, cash))
-    cash -= player_wager
-    amount_of_dice = input("How many dice would you like to roll?: ")
-    amount_of_dice = select_again(amount_of_dice)
-    amount_of_dice = int(amount_of_dice)
-    player_roll(amount_of_dice)
-    amount_player_wins = winning(dice, player_side)
-    if amount_player_wins == 2:
-        cash += playerwager*2
-    elif amount_player_wins >= 3:
-        cash += playerwager*3
-    if cash <= 0:
-        print("Game Over")
-        running = False
+def winner(dice ,given_bet):
+        win_multiplier = 0
+        if dice.count(given_bet) >= 2:
+                win_multiplier += dice.count(given_bet)
+        else:
+                print("You lose. ):")
+        return win_multiplier
+        
+roll_cost = 0
+
+while True:
+        print("You need a minimum of $10,000 to win.")
+        while running:
+                player_side = None
+                player_wager = 0
+                amount_player_wins = 0
+                dice.clear()
+                print(f"Cash: ${cash}")
+                dealing()
+                if forced_bet > 0:
+                        print(f"You were forced to bet ${forced_bet}")
+                print(f"1:{dice_rolled_one}/ 2:{dice_rolled_two}/ 3:{dice_rolled_three}/ 4:{dice_rolled_four}/ 5:{dice_rolled_five}/ 6:{dice_rolled_six}")    
+                player_side = int(pick_side(bet))
+                player_wager = int(gambling(wager, cash))
+                print(player_side)
+                cash -= player_wager
+                cash -= forced_bet
+                print(f"Cash: ${cash}")
+                print(f"Your cost to roll is: ${roll_cost}")
+                amount_of_dice = input("How many dice would you like to roll?: ")
+                amount_of_dice = select_again(amount_of_dice)
+                amount_of_dice = int(amount_of_dice)
+                player_roll(amount_of_dice)
+                cash -= roll_cost
+                if amount_of_dice == 2:
+                        roll_cost += random.randint(25, 50)
+                elif amount_of_dice == 3:
+                        roll_cost += random.randint(50, 100)
+                amount_player_wins = winner(dice, player_side)
+                if amount_player_wins >= 2:
+                        cash += (player_wager + forced_bet) * amount_player_wins
+                forced_bet += 10
+                dice_rolled_one += dice.count(1)
+                dice_rolled_two += dice.count(2)
+                dice_rolled_three += dice.count(3)
+                dice_rolled_four += dice.count(4)
+                dice_rolled_five += dice.count(5)
+                dice_rolled_six += dice.count(6)
+
+                if cash >= 10000:
+                        end_game = input("Would you like to continue?: ").capitalize()
+                        if end_game == "No":
+                         print("Congrats!!!")
+                         running = False
+                if cash <= 0:
+                        print("Game Over")
+                        running = False 
+
+        print("Final Score")
+        print("-----------")
+        print(f"Cash: {cash}")
+        restart = input("Restart(Y/N): ").capitalize()
+        if restart == "Y":
+                cash = 1000
+                forced_bet = 0
+                dice_rolled_one = 0
+                dice_rolled_two = 0
+                dice_rolled_three = 0
+                dice_rolled_four = 0
+                dice_rolled_five = 0
+                dice_rolled_six = 0
+                roll_cost = 0
+                running = True
+        else:
+                break
