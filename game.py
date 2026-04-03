bfa9a44c5f10ad642d8ec57259bcc07b1102b058
MindChallengerMath <mindchallengermath@gmail.com>
2026-04-03 13:44:46 -0400

    Update game.py
    Added multipliers and depending on highest cash earned.

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
-#Dice roller program.
+#Game of TwoThree.
 import random
-import math
 #● ┌ ─ ┐ │ └ ┘
 
 dice_art = {
@@ -38,9 +37,11 @@ dice_art = {
 
 running = True
 dice = []
-cash = 1000
+cash = 10000
 bet = None
 wager = 0
+bestscore = 0
+
 forced_bet = 0
 dice_rolled_one = 0
 dice_rolled_two = 0
@@ -65,7 +66,7 @@ def player_roll(amount_of_dice):
                         print()
 
 def pick_side(player_choice):
-        player_choice = input("What number do you bet on?: ")
+        player_choice = input("What number do you bet on(1-6)?: ")
         while player_choice.isdigit() == False:
                 print("That is not a number.")
                 player_choice = input("Pick side 1-6: ")
@@ -103,12 +104,30 @@ def winner(dice ,given_bet):
         else:
                 print("You lose. ):")
         return win_multiplier
-        
+def milestone(highscore):
+        mile = 1
+        if highscore >=  100000:
+                mile = 2.00
+        elif highscore >= 75000:
+                mile = 1.75
+        elif highscore >= 50000:
+                mile = 1.50
+        elif highscore >= 25000:
+                mile = 1.25 
+        return mile 
+def highscore(money, bestscore):
+        money_added_to_score = 0
+        if money > bestscore:
+                money_added_to_score += (money)
+        return money_added_to_score
+
+
 roll_cost = 0
 
 while True:
-        print("You need a minimum of $10,000 to win.")
+        print("You need a minimum of $100,000 to win.")
         while running:
+                milestone_multiplier = milestone(bestscore)
                 player_side = None
                 player_wager = 0
                 amount_player_wins = 0
@@ -124,28 +143,34 @@ while True:
                 cash -= player_wager
                 cash -= forced_bet
                 print(f"Cash: ${cash}")
-                print(f"Your cost to roll is: ${roll_cost}")
-                amount_of_dice = input("How many dice would you like to roll?: ")
+                if cash > 10000:
+                        print(f"Your cost to roll is: ${roll_cost}")
+                if milestone_multiplier > 1:
+                        print(f"Current Multiplier: {milestone_multiplier}")
+                amount_of_dice = input("Roll 2 or 3 dice?: ")
                 amount_of_dice = select_again(amount_of_dice)
                 amount_of_dice = int(amount_of_dice)
                 player_roll(amount_of_dice)
-                cash -= roll_cost
+                if cash > 10000:
+                        cash -= roll_cost
                 if amount_of_dice == 2:
                         roll_cost += random.randint(25, 50)
                 elif amount_of_dice == 3:
                         roll_cost += random.randint(50, 100)
                 amount_player_wins = winner(dice, player_side)
+                current_score = highscore(cash, bestscore)
                 if amount_player_wins >= 2:
-                        cash += (player_wager + forced_bet) * amount_player_wins
+                        cash += round(((player_wager + forced_bet) * amount_player_wins) * milestone_multiplier) 
                 forced_bet += 10
+                if current_score > 0:
+                        bestscore += current_score
                 dice_rolled_one += dice.count(1)
                 dice_rolled_two += dice.count(2)
                 dice_rolled_three += dice.count(3)
                 dice_rolled_four += dice.count(4)
                 dice_rolled_five += dice.count(5)
                 dice_rolled_six += dice.count(6)
-
-                if cash >= 10000:
+                if cash >= 100000:
                         end_game = input("Would you like to continue?: ").capitalize()
                         if end_game == "No":
                          print("Congrats!!!")
@@ -157,9 +182,11 @@ while True:
         print("Final Score")
         print("-----------")
         print(f"Cash: {cash}")
+        print("-----------")
         restart = input("Restart(Y/N): ").capitalize()
         if restart == "Y":
-                cash = 1000
+                cash = 10000
+                bestscore = 0
                 forced_bet = 0
                 dice_rolled_one = 0
                 dice_rolled_two = 0
